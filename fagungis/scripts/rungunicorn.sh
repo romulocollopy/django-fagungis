4062d2a59803da08d76501697e37ce1ea90f2dee
Carlos Leite <carlos.leite@znc.com.br>
2014-02-18 11:03:58 -0300

    gunicorn log file to standart logs path

--- fagungis/scripts/rungunicorn.sh
+++ fagungis/scripts/rungunicorn.sh
@@ -5,10 +5,12 @@ cd %(django_project_root)s
 # set PYTHONPATH to cwd
 export PYTHONPATH=`pwd`
 
+LOGFILE=%(django_user_home)s/logs/%(project)s_gunicorn.log
+
 source %(virtenv)s/bin/activate
 # start gunicorn with all options earlier declared in fabfile.py
 exec %(virtenv)s/bin/gunicorn_django -w %(gunicorn_workers)s \
     --user=%(django_user)s --group=%(django_user_group)s \
     --settings=%(django_project_settings)s \
     --bind=%(gunicorn_bind)s --log-level=%(gunicorn_loglevel)s \
-    --log-file=%(gunicorn_logfile)s 2>>%(gunicorn_logfile)s
+    --log-file=$LOGFILE 2>>$LOGFILE
