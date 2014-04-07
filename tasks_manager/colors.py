0f7584f18ab2cc9c8f5c435138ebc66d0d7e9317
Marcelo Tambalo <marcelo.zokis@gmail.com>
2014-04-07 18:09:17 -0300

    add metodos para puts_coloridos

--- tasks_manager/colors.py
+++ tasks_manager/colors.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from fabric.api import puts
 
 
 def _wrap_with(code):
@@ -10,6 +11,13 @@ def _wrap_with(code):
         return "\033[%sm%s\033[0m" % (c, text)
     return inner
 
+
+def puts_color(color):
+    def inner(text, *args, **kwargs):
+        return puts(color(text, *args, **kwargs))
+    return inner
+
+
 red = _wrap_with('31')
 green = _wrap_with('32')
 yellow = _wrap_with('33')
@@ -18,3 +26,11 @@ magenta = _wrap_with('35')
 cyan = _wrap_with('36')
 white = _wrap_with('37')
 bold = lambda str: '\033[1m%s\033[0m' % str
+
+puts_red = puts_color(red)
+puts_green = puts_color(green)
+puts_yellow = puts_color(yellow)
+puts_blue = puts_color(blue)
+puts_magenta = puts_color(magenta)
+puts_cyan = puts_color(cyan)
+puts_white = puts_color(white)
