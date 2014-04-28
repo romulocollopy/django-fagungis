2d108d92bdc1478187553ffa1c5c30031964acf6
Rômulo Collopy <romulocollopy@gmail.com>
2014-04-28 16:20:50 -0300

    Atualiza a chamada do gunicorn
    Atualiza a chamada do gunicorn para a forma prevista na documentação da ultima versão:
    http://gunicorn-docs.readthedocs.org/en/latest/run.html#django

--- fagungis/scripts/rungunicorn.sh
+++ fagungis/scripts/rungunicorn.sh
@@ -9,8 +9,6 @@ LOGFILE=%(django_user_home)s/logs/%(project)s_gunicorn.log
 
 source %(virtenv)s/bin/activate
 # start gunicorn with all options earlier declared in fabfile.py
-exec %(virtenv)s/bin/gunicorn_django -w %(gunicorn_workers)s \
-    --user=%(django_user)s --group=%(django_user_group)s \
-    --settings=%(django_project_settings)s \
-    --bind=%(gunicorn_bind)s --log-level=%(gunicorn_loglevel)s \
-    --log-file=$LOGFILE 2>>$LOGFILE
+
+# some problems were find with line breaks
+exec gunicorn -w %(gunicorn_workers)s --user=%(django_user)s --group=%(django_user_group)s --settings=%(django_project_settings)s --bind=%(gunicorn_bind)s --log-level=%(gunicorn_loglevel)s --log-file=%(gunicorn_logfile)s 2>>%(gunicorn_logfile)s %(project)s.wsgi:application
